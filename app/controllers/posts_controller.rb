164fdb5144f761b82d3e6c30d0fd17c4a789631d
K. Kanyuka <kkan.web@gmail.com>
2017-10-23 01:56:08 +0300

    Add endpoint for ips of several users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,10 +3,22 @@ class PostsController < ApplicationController
     post_form = PostForm.new(post_params)
     result = PostCreator.new(post_form).process
     if result.success?
-      render json: { post: result.objects[:post] }
+      post_fields = result.objects[:post].attributes.slice('id', 'title', 'content')
+      render json: { post: post_fields }
     else
       render json: { errors: result.errors }, code: 422
     end
+  end
+
+  def top
+    posts_number = params[:n].presence
+    posts = Post.top(posts_number).select(:id, :title, :content)
+    render json: { posts: posts }
+  end
+
+  def multiuser_ip_addresses
+    ips = IpUser.multiuser_ips.map{ |iu| { ip: iu.ip.to_s, logins: iu.logins } }
+    render json: { ips: ips }
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts, only: [:create]
+  resources :posts, only: [:create] do
+    get :top, on: :collection
+    get :multiuser_ip_addresses, on: :collection
+  end
   resources :reviews, only: [:create]
 end
