f3154cf6568c42a58b1f62e1f54a7f5a624ff206
K. Kanyuka <kkan.web@gmail.com>
2017-10-23 01:54:19 +0300

    Creating ip_user when adding post

--- app/services/post_creator.rb
+++ app/services/post_creator.rb
@@ -8,18 +8,19 @@ class PostCreator < ObjectCreator
   private
 
   def create_post_for_user
-    return @result.errors += @object_form.errors.full_messages unless @object_form.valid?
+    return @result.errors += @form_object.errors.full_messages unless @form_object.valid?
 
     ActiveRecord::Base.transaction(isolation: :serializable) do
-      user = User.find_or_create_by!(login: @object_form.login)
+      user = User.find_or_create_by!(login: @form_object.login)
       post = Post.create!(user: user,
-                          title: @object_form.title,
-                          content: @object_form.content,
-                          ip: @object_form.ip)
-      @result.objects.merge!(user: user, post: post)
+                          title: @form_object.title,
+                          content: @form_object.content,
+                          ip: @form_object.ip)
+      ip_user = IpUser.find_or_create_by!(user: user, ip: @form_object.ip)
+      @result.objects.merge!(user: user, post: post, ip_user: ip_user)
     end
   rescue ActiveRecord::StatementInvalid => e
     retry if e.message =~ /PG::TRSerializationFailure/
-    @result.errors << details: e.message
+    @result.errors << e.message
   end
 end
